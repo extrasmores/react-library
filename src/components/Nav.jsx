b0c0329de15cec17d34452721211eb7918d6bce7
Torin <extrasmores@gmail.com>
2023-08-04 14:56:26 -0600

    finishing library

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import LibraryLogo from '../assets/Library.svg'
 import { Link } from "react-router-dom";
 
-const Nav = () => {
+const Nav = ({ numberOfItems }) => {
     function openMenu() {
         document.body.classList += " menu--open";
     }
@@ -33,10 +33,12 @@ const Nav = () => {
             <FontAwesomeIcon icon="bars" />
         </button>
         <li className="nav__icon">
-            <Link to="" className="nav__link">
+            <Link to="/cart" className="nav__link">
                 <FontAwesomeIcon icon="shopping-cart" />
             </Link>
-            <span className="cart__length">2</span>
+            {
+              numberOfItems > 0 &&  <span className="cart__length">{numberOfItems}</span>
+            }
         </li>
         </ul>
         <div className="menu__backdrop" onClick={closeMenu}>
